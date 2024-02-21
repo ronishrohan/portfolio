b4c39e4bdee099c257eafa8457b4b89cf62d98e3
Roni <64074505+RonishRohan@users.noreply.github.com>
2024-02-21 14:17:49 +0530

    added animations

--- src/pages/home/components/footer/Footer.jsx
+++ src/pages/home/components/footer/Footer.jsx
@@ -13,18 +13,18 @@ function Footer() {
   });
   const scrollInItems = useScroll({
     target: sectionRef,
-    offset: ["0 1", "1 1"]
-  })
+    offset: ["0 1", "1 1"],
+  });
   const springInItems = useSpring(scrollInItems.scrollYProgress, {
-    damping: 20
-  })
+    damping: 20,
+  });
   const springIn = useSpring(scrollIn.scrollYProgress, {
     damping: 50,
     stiffness: 500,
   });
   const roundIn = useTransform(springIn, [0, 1], ["50%", "0%"]);
   const translateIn = useTransform(springIn, [0, 1], [200, 0]);
-  const translateItems = useTransform(springInItems,[0.5,1], ["100%", "0%"])
+  const translateItems = useTransform(springInItems, [0.5, 1], ["100%", "0%"]);
   return (
     <motion.footer
       style={{
@@ -37,13 +37,19 @@ function Footer() {
       className="w-full h-screen bg-black text-white flex flex-col shadow-[0px_0px_60px_rgba(0,0,0,0.2)]"
     >
       <Title translate={translateItems}>CONTACT</Title>
-      <div  className="w-full h-full grid grid-cols-[1fr_1fr] gap-8  relative overflow-hidden">
+      <div className="w-full h-full grid grid-cols-[1fr_1fr] gap-8  relative overflow-hidden">
         <FooterLinks translate={translateItems}></FooterLinks>
 
-        <motion.div style={{x:translateItems}} className="flex flex-col overflow-hidden h-full w-full gap-2">
-          <div className="overflow-hidden flex align-top border-4 border-orange_hover">
-            <img
-              className="top-0 h-auto w-full object-cover"
+        <motion.div
+          style={{ x: translateItems }}
+          className="flex flex-col overflow-hidden h-full w-full gap-2"
+        >
+          <div className="overflow-hidden flex border-4 border-orange_hover">
+            <motion.img
+              initial={{ y: "-25%", scale: 1.2 }}
+              whileHover={{ y: "-50%", scale: 1 }}
+              transition={{ type: "spring", damping: 50 }}
+              className="top-0 w-full h-fit "
               src="https://cdn.discordapp.com/attachments/976406861014257724/1209755032916860970/IMG_20230428_183208.jpg?ex=65e8132c&is=65d59e2c&hm=e465defb61e9f08ebd06de49584aa30ba39e192e6f3c851b15565efe67028a5c&"
               alt=""
             />
